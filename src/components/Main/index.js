0f3ee2795e6c7b7e52058a30244c16841e351288
josevm <villarnovojose@gmail.com>
2018-02-27 11:23:45 +1300

    home

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -4,12 +4,14 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Deals from './../../scenes/Deals';
+import Home from './../../scenes/Home';
 
 export default class Main extends Component {
     render() {
         return (
             <Switch>
-                <Route exact path={"/"} component={Deals}/>
+                <Route exact path={"/"} component={Home}/>
+                <Route exact path={"/deals"} component={Deals}/>
             </Switch>
         );
     }
---
+++ src/scenes/Home/index.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Joseph on 27/02/2018.
+ */
+import React, { Component } from 'react';
+
+
+export default class Home extends Component {
+
+    render() {
+        return (
+            <div>You are in HOME!</div>
+        );
+    }
+
+}
