55262b30cad1c60f0e3d9d4203c5ee1bca6a23b7
josevm <villarnovojose@gmail.com>
2018-03-15 18:15:44 +1300

    Setting a global configuration for the project

---
+++ src/config/index.js
@@ -0,0 +1,8 @@
+var globalConfig = {
+    facebook_auth_api: {
+        app_id: "424894577930611",
+        version: "v2.12",
+        sdk_url: 'https://connect.facebook.net/es_LA/sdk.js#xfbml=1&autoLogAppEvents=1&version=v2.12&appId=424894577930611'
+    }
+};
+export default globalConfig;
